bc96a86126d9ff90f76e47888b7e412e49de85df
Inna Kirianova <innr@ya.ru>
2022-11-21 23:40:01 +0300

    added code for task 11

---
+++ Task11/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+
+Console.WriteLine($"Случайное число: {number}");
+
+int dig1 = number/100;
+int dig3 = number%10;
+
+Console.Write(dig1);
+Console.Write(dig3);
